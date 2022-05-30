282837e3b1ec941e581f9431fcadbc9474bb874e
PatrickSilvaMenezes <patricksilvamenezes@gmail.com>
2022-05-30 15:12:59 -0300

    descendentes adicionados

--- AtividadeProlog_ArvoreGenealogica/Atividade01_Prolog.pl
+++ AtividadeProlog_ArvoreGenealogica/Atividade01_Prolog.pl
@@ -19,15 +19,18 @@ progenitor(maria, aristides).
 mae(X, Y) :- progenitor(X, Y), feminino(X). % X é projenitor de Y
 pai(X,Y)  :- progenitor(X,Y), masculino(X).% X é progenitor de Y
 
-irmao(X,Y):- progenitor(Z,X), progenitor(Z,Y), masculino(Y), X\==Y, !. % X é irmao de Y comparação de diferente é em vez de !=, \==
-irma(X,Y) :- progenitor(Z,X), progenitor(Z,Y), feminino(Y), X\==Y, !.% X é irma de Y
+irmao(X,Y):- progenitor(Z,X), progenitor(Z,Y), masculino(X), X\==Y, !. % X é irmao de Y comparação de diferente é em vez de !=, \==
+irma(X,Y) :- progenitor(Z,X), progenitor(Z,Y), feminino(X), X\==Y, !.% X é irma de Y
 
 avo(X,Y) :- progenitor(X,Z), progenitor(Z, Y), masculino(X) . % X é avo de Y
 
-tio(X,Y)  :- mae(S, Y), irmao(S, X); pai(S, Y), irmao(S, X), !. % funciona para achar tio, 
-tia(X,Y)  :- mae(S, Y), irma(S, X); pai(S, Y), irma(S, X), !. % funciona para achar tia,
+tio(X,Y)  :- mae(S, Y), irmao(X, S); pai(S, Y), irmao(X, S), !. % funciona para achar tio(X), 
+tia(X,Y)  :- mae(S, Y), irma(X, S); pai(S, Y), irma(X, S), !. % funciona para achar tia(X),
 tios(X,Y) :- tio(X,Y); tia(X,Y). % funciona para achar tio ou tia(X) de alguem (Y)
 
 primos(X,Y) :- tios(S,Y), progenitor(S,X). % funciona para achar os primos ou primas(X) de alguem(Y)
-%falta achar descendentes e ascendentes
+
+descendentes(X,Y) :- progenitor(Y,X).% tal que X é descendente de Y
+descendentes(X,Y) :- progenitor(Y,W), descendentes(X,W).
+
 
