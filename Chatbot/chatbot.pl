b7ec2581caaf4933f27f7180912a9596320d61b9
PatrickSilvaMenezes <patricksilvamenezes@gmail.com>
2022-06-22 22:46:28 -0300

    chatbot atualizado

--- Chatbot/chatbot.pl
+++ Chatbot/chatbot.pl
@@ -29,7 +29,7 @@ pw7:-
 	format("[2]Perfume Y"),nl,
 	format("[3]Loção pós barba"),nl,
     format("[4]Desodorante Z"),nl,
-    format("[5]Sabonete "),nl,
+    format("[5]Sabonete"),nl,
     format("[6]Batom Q"),nl,
     format("[7]Maquiagem A"),nl,
 	read(Resposta),
@@ -70,16 +70,6 @@ pw13:-
 pw14:-
     format("Produto devolvido"),nl,
     pw1.
-pw15:-
-    format("Aqui está a lista de produtos disponíveis para troca, Digite o número que deseja"),nl,
-	format("[1]Shampoo X"),nl,
-	format("[2]Perfume Y"),nl,
-	format("[3]Loção pós barba"),nl,
-    format("[4]Desodorante Z"),nl,
-    format("[5]Sabonete "),nl,
-    format("[6]Batom Q"),nl,
-    format("[7]Maquiagem A"),nl,
-    read(_Resposta).
 pw16:-
     format("Obrigado pela compra"),nl,
 	pw1.
@@ -93,36 +83,50 @@ pw18:-
 pw19:-
     format("Valor inválido"),nl,
 	pw1.
+pw20:-
+    format("Escolha o produto a ser trocado na loja"),nl,
+    format("[1]Shampoo Q"),nl,
+	format("[2]Perfume U"),nl,
+	format("[3]Loção pós barba L"),nl,
+    format("[4]Desodorante Y"),nl,
+    read(Resposta),
+    ans10(Resposta).
 %Parte das chamadas de respostas
 
 ans1(sair):- write_ln("Até mais!"),!.
 ans1(Resposta):- 
     (Resposta==sim) ->  pw7, fail;
+    (Resposta==quero)->  pw7, fail;
     (Resposta==nao) ->  pw2.
 
 ans2(sair):- write_ln("Até mais!"),!.
 ans2(Resposta):-
     (Resposta==sim) ->  pw9, fail;
+    (Resposta==quero) ->  pw9, fail;
     (Resposta==nao) ->  pw3.
 
 ans3(sair):- write_ln("Até mais!"),!.
 ans3(Resposta):-
     (Resposta==sim) ->  pw8, fail;
+    (Resposta==quero) ->  pw8, fail;
     (Resposta==nao) ->  pw4.
 
 ans4(sair):- write_ln("Até mais!"),!.
 ans4(Resposta):-
     (Resposta==sim) ->  pw10, fail;
+    (Resposta==quero) ->  pw10, fail;
     (Resposta==nao) ->  pw5.
 
 ans5(sair):- write_ln("Até mais!"),!.
 ans5(Resposta):-
     (Resposta==sim) ->  pw11, fail;
+    (Resposta==quero) ->  pw11, fail;
     (Resposta==nao) ->  pw6.
 
 ans6(sair):- write_ln("Até mais!"),!.
 ans6(Resposta):-
     (Resposta==sim) ->  pw12, fail;
+    (Resposta==quero) ->  pw12, fail;
     (Resposta==nao) ->  pw1.
 
 ans7(sair):- write_ln("Até mais!"),!.
@@ -137,9 +141,10 @@ ans8(Resposta):-
 
 ans9(sair):- write_ln("Até mais!"),!.
 ans9(Resposta):-
-    (Resposta >= 1 , Resposta <5) -> pw18, fail;
+    (Resposta >= 1 , Resposta <5) -> pw20, fail;
     (Resposta < 1 ; Resposta >4) -> pw19.
 
-
-    
-    
+ans10(sair):- write_ln("Até mais!"),!.
+ans10(Resposta):-
+	(Resposta >= 1 , Resposta <5) -> pw18, fail;
+    (Resposta < 1 ; Resposta >4) -> pw19.
